831949a81b742a1eacffd31d1b85c3e4675818f9
salmanAltaf101 <sllujaan44@gmail.com>
2020-07-09 16:50:47 +0500

    route has been added the app is ready for deployment.

--- public/text-editor-cpp/app-info/app-info.js
+++ public/text-editor-cpp/app-info/app-info.js
@@ -4,20 +4,36 @@
 
 var elem_version = document.querySelector("#version")
 var elem_download = document.querySelector("#download")
+var shot1 = document.querySelector(".shot-1")
+
+
+const app_versions = [
+    'v1.0.0'
+]
+const LATEST_VERSION = app_versions[0]
+
 
 
 
 const url = new URL(window.location.href)
+var version = url.searchParams.get('version')
 
-const version = url.searchParams.get('version')
+if(!version) version = LATEST_VERSION
 
-if(version) {
-    // do stuff
-    console.log(elem_version)
-    elem_version.innerHTML = `Version:       &nbsp;	&nbsp;	&nbsp;	&nbsp;${version}`
-    elem_download.innerHTML = `Download: text-editor-${version}`
-    elem_download.setAttribute('href', `https://github.com/sllujaan/text-editor/releases/download/${version}/setup.msi`)
+
+var valid_version = app_versions.find(ver => {return ver === version})
+if(!valid_version) {
+    alert("Invalid version was selected! The app has selected the latest version for you.")
+    version = LATEST_VERSION
+
 }
+
+// do stuff
+elem_version.innerHTML = `Version:       &nbsp;	&nbsp;	&nbsp;	&nbsp;${version}`
+elem_download.innerHTML = `Download: text-editor-${version}`
+elem_download.setAttribute('href', `https://github.com/sllujaan/text-editor/releases/download/${version}/setup.msi`)
+shot1.querySelector('img').setAttribute('src', `./assets/${version}.gif`)
+
 
 
 document.addEventListener('click', e => {
